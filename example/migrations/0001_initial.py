b4890245cbad1685493ae1c6f29372dcd2eddc44
Mishbah <mishbah@jp74.com>
2015-04-24 00:39:14 +0100

    Add migrations for `example` app — used for testing

---
+++ example/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Customer',
+            fields=[
+                ('user_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('first_name', models.CharField(max_length=30, verbose_name='first name', blank=True)),
+                ('last_name', models.CharField(max_length=30, verbose_name='last name', blank=True)),
+            ],
+            options={
+                'verbose_name': 'Customer',
+                'verbose_name_plural': 'Customers',
+            },
+            bases=('users.user',),
+        ),
+    ]
