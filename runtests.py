419846b2ea3f3b5ee73d91a875ba5b214516a252
Mishbah Razzaque <mishbahx@gmail.com>
2016-09-09 00:03:59 +0100

    Drop travis config for django 1.5/1/6/1.7

--- runtests.py
+++ runtests.py
@@ -36,6 +36,24 @@ try:
         NOSE_ARGS=['-s'],
         AUTH_USER_MODEL='users.User',
         USERS_CREATE_SUPERUSER=False,
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.contrib.auth.context_processors.auth',
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.i18n',
+                        'django.template.context_processors.media',
+                        'django.template.context_processors.static',
+                        'django.template.context_processors.tz',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ]
     )
 
     try:
